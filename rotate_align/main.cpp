815ffa84d31c021daa5a0e6566cf56aa110d9388
Pedro Raimundo <pedro.raimundo@fieb.org.br>
2018-06-13 18:34:14 -0300

    Beginnings of mesh rotation (and, in the future, alignment) using PCL

---
+++ rotate_align/main.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <pcl/io/ply_io.h>
+#include <pcl/point_types.h>
+#include <pcl/point_cloud.h>
+#include <pcl/common/transforms.h>
+#include <pcl/visualization/pcl_visualizer.h>
+
+int
+main (int argc, char** argv)
+{
+  float theta = M_PI/4;
+  pcl::PointCloud<pcl::PointXYZRGBNormal>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZRGBNormal>);
+
+  if (pcl::io::loadPLYFile<pcl::PointXYZRGBNormal> ("dataset-estatua-sr/estatua_srmesh_0.ply", *cloud) == -1) //* load the file
+  {
+    PCL_ERROR ("Couldn't read file test_pcd.pcd \n");
+    return (-1);
+  }
+  std::cout << "Loaded "
+            << cloud->width * cloud->height
+            << " data points from test_pcd.pcd with the following fields: "
+            << std::endl;
+  for (size_t i = 0; i < cloud->points.size (); ++i)
+    std::cout << "    " << cloud->points[i].x
+              << " "    << cloud->points[i].y
+              << " "    << cloud->points[i].z
+              << " "    << (int)cloud->points[i].r
+              << " "    << (int)cloud->points[i].g
+              << " "    << (int)cloud->points[i].b
+              << " "    << cloud->points[i].normal_x
+              << " "    << cloud->points[i].normal_y
+              << " "    << cloud->points[i].normal_z << std::endl;
+
+  /*  METHOD #2: Using a Affine3f
+    This method is easier and less error prone
+  */
+  Eigen::Affine3f transform_2 = Eigen::Affine3f::Identity();
+
+  // Define a translation of 2.5 meters on the x axis.
+  transform_2.translation() << 2.5, 0.0, 0.0;
+
+  // The same rotation matrix as before; theta radians around Z axis
+  transform_2.rotate (Eigen::AngleAxisf (theta, Eigen::Vector3f::UnitZ()));
+
+  // Print the transformation
+  printf ("\nMethod #2: using an Affine3f\n");
+  std::cout << transform_2.matrix() << std::endl;
+
+  // Executing the transformation
+  pcl::PointCloud<pcl::PointXYZRGBNormal>::Ptr transformed_cloud (new pcl::PointCloud<pcl::PointXYZRGBNormal> ());
+  // You can either apply transform_1 or transform_2; they are the same
+  pcl::transformPointCloud (*cloud, *transformed_cloud, transform_2);
+
+  printf(  "\nPoint cloud colors :  white  = original point cloud\n"
+      "                        red  = transformed point cloud\n");
+  pcl::visualization::PCLVisualizer viewer ("Matrix transformation example");
+
+   // Define R,G,B colors for the point cloud
+  pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZRGBNormal> cloud_color_handler (cloud, 255, 255, 255);
+  // We add the point cloud to the viewer and pass the color handler
+  viewer.addPointCloud (cloud, cloud_color_handler, "original_cloud");
+
+  pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZRGBNormal> transformed_cloud_color_handler (transformed_cloud, 230, 20, 20); // Red
+  viewer.addPointCloud (transformed_cloud, transformed_cloud_color_handler, "transformed_cloud");
+
+  viewer.addCoordinateSystem (1.0, "cloud", 0);
+  viewer.setBackgroundColor(0.05, 0.05, 0.05, 0); // Setting background to a dark grey
+  viewer.setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 2, "original_cloud");
+  viewer.setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 2, "transformed_cloud");
+  //viewer.setPosition(800, 400); // Setting visualiser window position
+
+  while (!viewer.wasStopped ()) { // Display the visualiser until 'q' key is pressed
+    viewer.spinOnce ();
+  }
+
+  return (0);
+}
